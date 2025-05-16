69b092403e32d179d08f0d50c73ea5d765614ac4
Dave23377777 <23751662@stu.mmu.ac.uk>
2025-05-16 23:15:57 +0100

    mjhg

--- FUCKINGHELL/src/view/MainTableFrame.java
+++ FUCKINGHELL/src/view/MainTableFrame.java
@@ -971,7 +971,7 @@ private void setDelayPanel() {
 			fullPanel.add(scrollPane);
 		}
 		userFuncPanel.removeAll();
-//dab
+//dabhhh
 		JLabel termLabel = new JLabel("Search:");
 		JTextField term = new JTextField(20); // set size to roughly 20 characters
 		JLabel in = new JLabel("In:");
